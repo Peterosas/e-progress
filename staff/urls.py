3352004fd3675b467bbcbceae0ef3f44f3b42865
unknown <paulodion96@gmail.com>
2024-08-02 02:25:21 +0100

    Navigate between pages

--- staff/urls.py
+++ staff/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('login/', Login, name='s_login'),
     path('dashboard/overview/', DashboardOverview, name='dashboard_overview'),
     path('dashboard/results/', PrintResults, name='print_results'),
+    path('dashboard/my-profile/', PrintResults, name='my_profile'),
+    path('dashboard/my-courses/', PrintResults, name='my_courses'),
 ]
